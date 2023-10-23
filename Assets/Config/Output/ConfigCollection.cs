87e71bb522323372d80acaf7ba5361b0e0c248cc
Horzine <1405261452@qq.com>
2023-10-23 22:41:17 +0800

    Add UniTask

--- Assets/Config/Output/ConfigCollection.cs
+++ Assets/Config/Output/ConfigCollection.cs
@@ -11,6 +11,10 @@ namespace XiConfig
         private const string kLoadFloder = ConfigUtils.kRuntimLoadFolder;
         public Dictionary<string, Template> AllTemplate { get; } = new();
 
-        public void Init() => ConfigUtils.ParseConfigData(File.ReadAllLines(Path.Combine(kLoadFloder, "Template.txt")), AllTemplate);
+        public void Init()
+        {
+            ConfigUtils.ParseConfigData(File.ReadAllLines(Path.Combine(kLoadFloder, "Template.txt")), AllTemplate);
+
+        }
     }
 }
