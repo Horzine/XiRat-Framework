212e99d6b369f19f751803573d1483d179ab275b
Horzine <1405261452@qq.com>
2023-11-03 15:37:03 +0800

    Test Deque & MinMaxHeap

---
+++ Assets/Scripts/Test Case/Collection/Test_Deque.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using Xi.Extend.Collection;
+
+namespace Xi.TestCase
+{
+    public class Test_Deque : MonoBehaviour
+    {
+        private void Start()
+        {
+            var deque = new Deque<int>();
+
+            deque.AddToBack(0);
+            deque.AddToBack(1);
+            deque.AddToBack(2);
+            deque.AddToBack(3);
+            deque.AddToBack(4);
+            deque.AddToFront(-1);
+            deque.AddToFront(-2);
+            deque.AddToFront(-3);
+            deque.AddToFront(-4);
+            Debug.Log(deque.RemoveFromBack());
+            Debug.Log(deque.RemoveFromFront());
+            Debug.Log(deque.RemoveFromBack());
+            Debug.Log(deque.RemoveFromFront());
+            Debug.Log(deque.RemoveFromBack());
+            Debug.Log(deque.RemoveFromFront());
+            Debug.Log(deque.RemoveFromBack());
+            Debug.Log(deque.RemoveFromFront());
+            Debug.Log(deque.RemoveFromBack());
+            Debug.Log(deque.RemoveFromFront());
+        }
+    }
+}
---
+++ Assets/Scripts/Test Case/Collection/Test_MinMaxHeap.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using Xi.Extend.Collection;
+
+namespace Xi.TestCase
+{
+    public class Test_MinMaxHeap : MonoBehaviour
+    {
+        private void Start()
+        {
+            var minHeap = new MinHeap<int>
+            {
+                2,
+                3,
+                4,
+                1,
+                1,
+                5,
+                -1,
+            };
+
+            Debug.Log(minHeap.ExtractMin());
+            Debug.Log(minHeap.ExtractMin());
+            Debug.Log(minHeap.ExtractMin());
+            Debug.Log(minHeap.ExtractMin());
+            Debug.Log(minHeap.ExtractMin());
+            Debug.Log(minHeap.ExtractMin());
+            Debug.Log(minHeap.ExtractMin());
+
+            var heap2 = new MinHeap<string, int>
+            {
+                { "item1", 3 },
+                { "item2", 4 },
+                { "item3", 5 }
+            };
+            var curMin = heap2.Min;
+            Debug.Log($"{curMin.Key}, {curMin.Value}");
+            heap2.ChangeValue("item2", 1);   // Now value of "item2" is 1.
+            var item = heap2.ExtractMin();      // Returns 1.
+            Debug.Log($"{item.Key}, {item.Value}");
+        }
+    }
+}
