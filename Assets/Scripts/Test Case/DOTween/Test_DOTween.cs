c5ecb579f79c59c7e6552b5667f6af1cf4adc436
Horzine <1405261452@qq.com>
2023-11-03 14:39:59 +0800

    Add Test_DOTween

---
+++ Assets/Scripts/Test Case/DOTween/Test_DOTween.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+namespace Xi.TestCase
+{
+    public class Test_DOTween : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start() => transform.DOMove(new Vector3(1, 2, 3), 3);
+
+        // Update is called once per frame
+        private void Update()
+        {
+
+        }
+    }
+}
