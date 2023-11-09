e59f14621511391a41508c98392866742642c777
Horzine <1405261452@qq.com>
2023-11-09 19:12:04 +0800

    Test Input System wip

---
+++ Assets/Scripts/Test Case/Input System/Test_InputSystem_Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using static UnityEngine.InputSystem.InputAction;
+
+namespace Xi.TestCase
+{
+    public class Test_InputSystem_Player : MonoBehaviour
+    {
+        public void InputMove(CallbackContext context)
+        {
+            var moveInput = context.ReadValue<Vector2>();
+            print($"InputMove: {moveInput}");
+        }
+
+        private void Update() => KeyWasPressedThisFrame();
+
+        private void KeyWasPressedThisFrame() => print(Keyboard.current.spaceKey.wasPressedThisFrame);
+
+
+        void Test()
+        {
+            var playerInput = GetComponent<PlayerInput>();
+           var reloadAction =  playerInput.actions.FindActionMap("player").FindAction("Reload");
+        }
+    }
+}
