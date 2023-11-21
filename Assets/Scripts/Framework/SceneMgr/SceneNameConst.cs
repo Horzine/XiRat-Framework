4228564e22900776ea8adeeac79e14c6d3cbb48d
Horzine <1405261452@qq.com>
2023-11-21 14:52:12 +0800

    SceneNameConst

---
+++ Assets/Scripts/Framework/SceneMgr/SceneNameConst.cs
@@ -0,0 +1,13 @@
+﻿namespace Xi.Framework
+{
+    public static class SceneNameConst
+    {
+        public const string kAddressableGroupName = "SceneInBuild";
+        public const string kBoost = "Boost";
+        public const string kMain = "Main";
+        public const string kTest_Case = "Test_Case";
+        public const string kMap_1 = "Map_1";
+
+        public static string AddressableName(string sceneName) => $"{kAddressableGroupName}/{sceneName}";
+    }
+}
