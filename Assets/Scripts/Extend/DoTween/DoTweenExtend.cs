c25e207cee1532216ff7a5f2d315e6e72eacca5a
Horzine <1405261452@qq.com>
2023-12-22 17:28:40 +0800

    DoTweenExtend

---
+++ Assets/Scripts/Extend/DoTween/DoTweenExtend.cs
@@ -0,0 +1,15 @@
+﻿using DG.Tweening;
+
+namespace Xi.Extend.DoTween
+{
+    public static class DoTweenExtend
+    {
+        public static void SafeKill(this Tween tween)
+        {
+            if (tween != null && tween.IsActive())
+            {
+                tween.Kill();
+            }
+        }
+    }
+}
