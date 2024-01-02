7876aa7a4868cb593dbed3cb41bab1c936d39ca8
Horzine <1405261452@qq.com>
2024-01-02 12:56:32 +0800

    IndexedSet

---
+++ Assets/Scripts/Extend/Collection/IndexedSet/IndexedSet.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Xi.Extend.Collection
+{
+    public class IndexedSet<T> : IList<T>
+    {
+        private readonly List<T> _elements = new();
+        private readonly Dictionary<T, int> _hashedElementIdx = new();
+
+        public void Add(T item)
+        {
+            if (!_hashedElementIdx.ContainsKey(item))
+            {
+                int index = _elements.Count;
+                _hashedElementIdx.Add(item, index);
+                _elements.Add(item);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            if (_hashedElementIdx.TryGetValue(item, out int index))
+            {
+                RemoveAt(index);
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Remove(Predicate<T> match)
+        {
+            _elements.RemoveAll(match);
+            RefreshIndices();
+        }
+
+        public void Clear()
+        {
+            _elements.Clear();
+            _hashedElementIdx.Clear();
+        }
+
+        public bool Contains(T item) => _hashedElementIdx.ContainsKey(item);
+
+        public int Count => _elements.Count;
+
+        public bool IsReadOnly => false;
+
+        public int IndexOf(T item) => _hashedElementIdx.TryGetValue(item, out int index) ? index : -1;
+
+        public void Insert(int index, T item) => throw new NotSupportedException("Random Insertion is semantically invalid, since this structure does not guarantee ordering.");
+
+        public void RemoveAt(int index)
+        {
+            var removedItem = _elements[index];
+            _elements.RemoveAt(index);
+            _hashedElementIdx.Remove(removedItem);
+
+            if (index != _elements.Count)
+            {
+                var lastItem = _elements[index];
+                _hashedElementIdx[lastItem] = index;
+            }
+        }
+
+        public T this[int index]
+        {
+            get => _elements[index];
+            set
+            {
+                var oldItem = _elements[index];
+                _hashedElementIdx.Remove(oldItem);
+
+                _elements[index] = value;
+                _hashedElementIdx.Add(value, index);
+            }
+        }
+
+        public void Sort(Comparison<T> sortLayoutFunction)
+        {
+            _elements.Sort(sortLayoutFunction);
+            RefreshIndices();
+        }
+
+        public IEnumerator<T> GetEnumerator() => _elements.GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        void ICollection<T>.CopyTo(T[] array, int arrayIndex) => _elements.CopyTo(array, arrayIndex);
+
+        private void RefreshIndices()
+        {
+            _hashedElementIdx.Clear();
+            for (int i = 0; i < _elements.Count; ++i)
+            {
+                var item = _elements[i];
+                _hashedElementIdx.Add(item, i);
+            }
+        }
+    }
+}
---
+++ Assets/Test Case/Collection/Test_IndexedSet.cs
@@ -0,0 +1,145 @@
+﻿using UnityEngine;
+using UnityEngine.Assertions;
+using Xi.Extend.Collection;
+
+namespace Xi.TestCase
+{
+    public class Test_IndexedSet : MonoBehaviour
+    {
+        private void Start()
+        {
+            print("Test Start");
+
+            AddElement_AddsToSet();
+
+            RemoveElement_RemovesFromSet();
+
+            RemoveNonExistentElement_ReturnsFalse();
+
+            RemoveByPredicate_RemovesMatchingElements();
+
+            Clear_EmptySet();
+
+            IndexOf_ReturnsCorrectIndex();
+
+            Sort_SortsElements();
+
+            GetEnumerator_ReturnsEnumerator();
+
+            print("Test End");
+        }
+
+        public void AddElement_AddsToSet()
+        {
+            var set = new IndexedSet<int>
+            {
+                1
+            };
+            Assert.IsTrue(set.Contains(1));
+            Assert.AreEqual(1, set.Count);
+        }
+
+        public void RemoveElement_RemovesFromSet()
+        {
+            var set = new IndexedSet<int>
+            {
+                1
+            };
+            Assert.IsTrue(set.Remove(1));
+            Assert.IsFalse(set.Contains(1));
+            Assert.AreEqual(0, set.Count);
+        }
+
+        public void RemoveNonExistentElement_ReturnsFalse()
+        {
+            var set = new IndexedSet<int>();
+            Assert.IsFalse(set.Remove(1));
+        }
+
+        public void RemoveByPredicate_RemovesMatchingElements()
+        {
+            var set = new IndexedSet<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            set.Remove(x => x % 2 == 0);
+
+            Assert.IsFalse(set.Contains(2));
+            Assert.IsTrue(set.Contains(1));
+            Assert.IsTrue(set.Contains(3));
+            Assert.AreEqual(2, set.Count);
+        }
+
+        public void Clear_EmptySet()
+        {
+            var set = new IndexedSet<int>
+            {
+                1,
+                2
+            };
+
+            set.Clear();
+
+            Assert.AreEqual(0, set.Count);
+            Assert.IsFalse(set.Contains(1));
+            Assert.IsFalse(set.Contains(2));
+        }
+
+        public void IndexOf_ReturnsCorrectIndex()
+        {
+            var set = new IndexedSet<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            Assert.AreEqual(0, set.IndexOf(1));
+            Assert.AreEqual(1, set.IndexOf(2));
+            Assert.AreEqual(2, set.IndexOf(3));
+            Assert.AreEqual(-1, set.IndexOf(4));
+        }
+
+        public void Sort_SortsElements()
+        {
+            var set = new IndexedSet<int>
+            {
+                3,
+                1,
+                2
+            };
+
+            set.Sort((a, b) => a.CompareTo(b));
+
+            Assert.AreEqual(1, set[0]);
+            Assert.AreEqual(2, set[1]);
+            Assert.AreEqual(3, set[2]);
+        }
+
+        public void GetEnumerator_ReturnsEnumerator()
+        {
+            var set = new IndexedSet<int>
+            {
+                1,
+                2,
+                3
+            };
+
+            var enumerator = set.GetEnumerator();
+
+            Assert.IsTrue(enumerator.MoveNext());
+            Assert.AreEqual(1, enumerator.Current);
+
+            enumerator.MoveNext();
+            Assert.AreEqual(2, enumerator.Current);
+
+            enumerator.MoveNext();
+            Assert.AreEqual(3, enumerator.Current);
+
+            Assert.IsFalse(enumerator.MoveNext());
+        }
+    }
+}
