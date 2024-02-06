456afd33a84eac9acee9ba0a1d7d10b1bb54e1b0
Horzine <1405261452@qq.com>
2024-02-06 12:27:04 +0800

    refactor wip

--- Assets/Scripts/Metagame/Ui/ClassBuild/ClassBuildWindowController.cs
+++ Assets/Scripts/Metagame/Ui/ClassBuild/ClassBuildWindowController.cs
@@ -1,19 +1,22 @@
-﻿using Cysharp.Threading.Tasks;
-using Xi.Framework;
+﻿using Xi.Framework;
+using static Xi.Metagame.Ui.ClassBuildWindowController;
 
 namespace Xi.Metagame.Ui
 {
-    public class ClassBuildWindowController : UiBaseController<ClassBuildWindow>
+    public class ClassBuildWindowController : UiBaseController<ClassBuildWindow, InitParams>
     {
-        protected override UiEnum UiEnum => UiEnum.Metagame_ClassBuild;
+        public struct InitParams : IUiInitParams
+        {
+
+        }
+        protected override UiEnum UiEnumValue => UiEnum.Metagame_ClassBuild;
         protected override (string groupName, string uiFeatureName, string uiPrefabName) PrefabAssetPath
             => (AssetGroupNameConst.kAddressableGroupName_MetagameUi, UiFeatureNameConst.kMetagame_ClassBuild, UiPrefabNameConst.kMetagame_ClassBuild);
         protected override bool IsOverlayMode => false;
 
-        protected override void CleanControllerDependency() { }
         protected override void OnCloseAccomplishCallback() { }
         protected override void OnOpenAccomplishCallback() { }
-
-        public void Open() => OpenAsync().Forget();
+        protected override void OnWindowInstantiateCallback() { }
+        protected override void OnWindowDestoryCallback() { }
     }
 }
